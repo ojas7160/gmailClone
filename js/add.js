0b7c68c690b5636b1026e566f795641fb8333084
Ojaswi wadhwa <ojas.vwadhwa1995@gmail.com>
2017-08-25 07:46:56 +0530

    Added

--- js/add.js
+++ js/add.js
@@ -1,4 +1,17 @@
 app.controller("addctrl", function($scope){
    $scope.add = "Add here.."; 
     
+     $scope.saved = localStorage.getItem("label");
+    
+    $scope.label = (localStorage.getItem("label")!==null) ? 
+        JSON.parse($scope.saved) : [{text : "Job", done : false}, {text : "complaint", done : false}];
+    
+    localStorage.setItem("label", JSON.stringify($scope.label));
+    
+    $scope.addLabel = function(){
+       //alert("hii");
+        $scope.label.push({text : $scope.labelAdd, done : false});
+        localStorage.setItem("label", JSON.stringify($scope.label));
+    };
+    
 });
--- js/compose.js
+++ js/compose.js
@@ -1,3 +1,16 @@
 app.controller("composectrl", function($scope){
    $scope.compose = "Compose a new mail here.."; 
+    
+ $scope.saved = localStorage.getItem("list");
+    
+    $scope.list = (localStorage.getItem("list")!==null) ? 
+        JSON.parse($scope.saved) : [{subject1 : "Job", done : false}, {subject1 : "complaint", done : false}];
+    
+    localStorage.setItem("list", JSON.stringify($scope.list));
+    
+    $scope.addEmail = function(){
+        //alert("hi");
+        $scope.list.push({subject1 : $scope.emailSubject, done : false});
+        localStorage.setItem("list", JSON.stringify($scope.list));
+    };
 });
